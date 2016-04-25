18a5b8e434ba0f3bc8f664645a74b8c53a493e2f
PatrickCooke <cookepa1@gmail.com>
2016-04-25 12:55:44 -0400

    basic search/pull function working. created readable array of JSON info

--- TuneSearch/ViewController.m
+++ TuneSearch/ViewController.m
@@ -7,21 +7,126 @@
 //
 
 #import "ViewController.h"
+#import "Reachability.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSString *hostName;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - Global Variables
+
+Reachability *hostReach;
+Reachability *internetReach;
+Reachability *wifiReach;
+bool internetAvailable;
+bool serverAvailable;
+
+#pragma mark - Interactivity Methods
+
+-(IBAction)getFilePressed:(id)sender {
+    NSLog(@"Get File");
+    if (serverAvailable) {
+        NSLog(@"Server Available");
+        NSURL *fileURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://%@/search?term=jack+johnson", _hostName]];
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+        [request setURL:fileURL];
+        [request setCachePolicy:NSURLRequestReloadIgnoringCacheData];
+        [request setTimeoutInterval:30.0];
+        NSURLSession *session = [NSURLSession sharedSession];
+        [[session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            NSLog(@"Got Response");
+            if (([data length] > 0) && (error == nil)) {
+                //NSLog(@"got data %@", data);
+                //NSString *dataString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                //NSLog(@"Got String %@", dataString);
+                NSJSONSerialization *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+                //NSLog(@"Got jSON %@", json);
+                NSArray *resultsArray = [(NSDictionary *)json objectForKey:@"results"];
+                for (NSDictionary *resultsDict in resultsArray) {
+                    NSLog(@"Results %@ - %@", [resultsDict objectForKey:@"artistName"],[resultsDict objectForKey:@"trackName"]);
+                }
+            }
+        }] resume];
+        
+    } else {
+        NSLog(@"Server is not Available");
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Server not Available" message:@"Internet Unavailable, please connect or contact Admin" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style: UIAlertActionStyleDefault handler:nil];
+        [alert addAction: okAction];
+        [self presentViewController:alert animated:true completion:nil];
+    }
+}
+
+
+
+
+#pragma mark - Network Methods
+
+-(void)updateReachabilityStatus:(Reachability *) currentReach { //this method is called anything the network type changes
+    NSParameterAssert([currentReach isKindOfClass:[Reachability class]]); //this code makes sure that "currentReach" is actually a Reachablity class
+    NetworkStatus netStatus = [currentReach currentReachabilityStatus];
+    if (currentReach == hostReach) {
+        switch (netStatus) { //this series makes sure if the server is up
+            case NotReachable:
+                NSLog(@"Sever Not Available");
+                serverAvailable = false;
+                break;
+            case ReachableViaWWAN:
+                NSLog(@"Server Reachable via WWAN");
+                serverAvailable = true;
+            case ReachableViaWiFi:
+                NSLog(@"Server Reachable via WiFi");
+                serverAvailable = true;
+            default:
+                break;
+        }
+    }
+    if (currentReach == internetReach || currentReach == wifiReach) {
+        switch (netStatus) {
+            case NotReachable:
+                NSLog(@"Internet not Available");
+                internetAvailable = false;
+                break;
+            case ReachableViaWWAN:
+                NSLog(@"Internet Available via WWAN");
+                internetAvailable = true;
+            case ReachableViaWiFi:
+                NSLog(@"Internet Available via WiFi");
+                internetAvailable = true;
+            default:
+                break;
+        }
+    }
+}
+
+
+-(void)reachablityChanged:(NSNotification *)notification {
+    Reachability *currentReach = [notification object];
+    [self updateReachabilityStatus:currentReach];
+}
+
+#pragma mark - Life Cycle Methods
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _hostName = @"itunes.apple.com";
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reachablityChanged:) name:kReachabilityChangedNotification object:nil];
+    hostReach = [Reachability reachabilityWithHostname:_hostName];
+    [hostReach startNotifier];
+    
+    internetReach = [Reachability reachabilityForInternetConnection];
+    [internetReach startNotifier];
+    
+    wifiReach = [Reachability reachabilityForLocalWiFi];
+    [wifiReach startNotifier];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
